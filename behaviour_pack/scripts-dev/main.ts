13c1da5bf1bbb2e73cd3bb703c536f74a9df32df
Pavel <pbkrutov@gmail.com>
2025-06-20 18:18:56 +0300

    remove server stuff

--- behaviour_pack/scripts-dev/main.ts
+++ behaviour_pack/scripts-dev/main.ts
@@ -1,6 +1,5 @@
 import load_custom_components from './components';
 import load_loops from './loops'
-import load_world_event_handlers from './events';
 import {WorldCache} from "./api/sacrifice";
 
 const guild_id = process.env.GUILD_ID || '0'
@@ -17,4 +16,3 @@ load_custom_components(guild_id)
 
 // Load Event Handlers
 // These are handlers for game events such as join, leave, break blocks, etc.
-load_world_event_handlers(guild_id)
