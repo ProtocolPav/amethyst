5bf0125cee68c84d6390d2e1112112f95129c81b
Pavel <pbkrutov@gmail.com>
2025-09-08 16:17:24 +0300

    add cage and anvil

--- behaviour_pack/scripts-dev/components/lucky.ts
+++ behaviour_pack/scripts-dev/components/lucky.ts
@@ -18,14 +18,25 @@ export default function load_lucky_component() {
     function lucky_block_break(event : BlockComponentPlayerBreakEvent) {
         const mainhand = event.player?.getComponent(EntityComponentTypes.Equippable)?.getEquipment(EquipmentSlot.Mainhand)
         if (!mainhand?.getComponent(ItemComponentTypes.Enchantable)?.hasEnchantment(MinecraftEnchantmentTypes.SilkTouch)) {
-            const scarecrow = world.structureManager.get('trap_cage')
-            const spawn_location = {
-                x: Math.round(event.player?.location.x - (scarecrow?.size.x / 2)),
-                y: event.player?.location.y - 1,
-                z: Math.round(event.player?.location.z - (scarecrow?.size.z / 2))
-            }
-            world.structureManager.place(scarecrow, event.dimension, spawn_location)
+            spawn_cage(event)
         }
+    }
+
+    function spawn_cage(event : BlockComponentPlayerBreakEvent) {
+        const scarecrow = world.structureManager.get('trap_cage')
+        const spawn_location = {
+            x: Math.round(event.player?.location.x - (scarecrow?.size.x / 2)),
+            y: event.player?.location.y - 1,
+            z: Math.round(event.player?.location.z - (scarecrow?.size.z / 2))
+        }
+        world.structureManager.place(scarecrow, event.dimension, spawn_location)
+
+        const anvil_location = {
+            x: event.player?.location.x,
+            y: event.player?.location.y + 30,
+            z: event.player?.location.z
+        }
+        event.dimension.setBlockType(anvil_location, MinecraftBlockTypes.Anvil)
     }
     
     system.beforeEvents.startup.subscribe(initEvent => {
