265d42cf51f1dff421f26e975803f27cfc27b30a
Pavel <pbkrutov@gmail.com>
2025-12-19 17:59:36 +0200

    refactor: bump version and bundle

--- behaviour_pack/scripts/main.js
+++ behaviour_pack/scripts/main.js
@@ -6176,7 +6176,7 @@ function load_world_event_handlers(guild_id2) {
 
 // behaviour_pack/scripts-dev/main.ts
 import { system as system19 } from "@minecraft/server";
-var guild_id = "1213827104945471538";
+var guild_id = "611008530077712395";
 WorldCache.load_world(guild_id).then();
 load_loops();
 load_custom_components(guild_id);
