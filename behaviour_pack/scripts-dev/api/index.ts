b7a7940dd64293274ffa998dcea48f0233db843a
Pavel <pbkrutov@gmail.com>
2025-12-23 20:35:09 +0200

    refactor: use new Quest and QuestProgress in API

--- behaviour_pack/scripts-dev/api/index.ts
+++ behaviour_pack/scripts-dev/api/index.ts
@@ -1,16 +1,16 @@
 import ThornyUser from "./user";
 import Relay from "./relay";
 import Interaction from "./interaction";
-import Quest from "./quest";
-import QuestWithProgress from "./quest_with_progress";
 import {Item, World} from "./sacrifice";
+import Quest from "./quests/quest";
+import QuestProgress from "./quests/quest_progress";
 
 const api = {
     ThornyUser,
     Relay,
     Interaction,
     Quest,
-    QuestWithProgress,
+    QuestProgress,
     Item,
     World
 };
