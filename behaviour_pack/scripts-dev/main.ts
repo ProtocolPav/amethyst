c90502346a21166ddd612bd3a79b62e21a6aae22
Pavel Krutov <pavel@krutov.eu>
2026-03-01 02:24:45 +0000

    fix: lore command execution

--- behaviour_pack/scripts-dev/main.ts
+++ behaviour_pack/scripts-dev/main.ts
@@ -8,11 +8,9 @@ import {
     CustomCommand,
     CustomCommandParamType,
     CustomCommandStatus, EntityComponentTypes,
-    EntityInventoryComponent, EquipmentSlot,
+    EquipmentSlot,
     system
 } from "@minecraft/server";
-
-import {ArgumentOutOfBoundsError} from "@minecraft/common"
 
 const guild_id = process.env.GUILD_ID || '0'
 
@@ -41,69 +39,33 @@ system.beforeEvents.startup.subscribe(initEvent => {
     })
 
     const loreCommand: CustomCommand = {
-        name: "amethyst:loreadd",
-        description: "Add one line of lore to the item you're holding",
-        permissionLevel: CommandPermissionLevel.Admin,
-        mandatoryParameters: [{ type: CustomCommandParamType.String, name: "text" }],
+        name: "amethyst:lore",
+        description: "Add a line to the lore, or remove all lore if blank",
+        permissionLevel: CommandPermissionLevel.GameDirectors,
+        optionalParameters: [{ type: CustomCommandParamType.String, name: "text" }],
     };
 
     initEvent.customCommandRegistry.registerCommand(
         loreCommand,
         (origin, ...args) => {
             try {
-                const mainhand = origin.initiator?.getComponent(EntityComponentTypes.Equippable)?.getEquipment(EquipmentSlot.Mainhand)
+                const mainhand = origin.sourceEntity?.getComponent(EntityComponentTypes.Equippable)?.getEquipment(EquipmentSlot.Mainhand)
 
-                const lore = mainhand?.getLore()
-                lore?.push(args[0])
-                mainhand?.setLore(lore)
-
-                origin.initiator?.getComponent(EntityComponentTypes.Equippable)?.setEquipment(EquipmentSlot.Mainhand, mainhand)
-            } catch (e: any) {
-                if (e instanceof ArgumentOutOfBoundsError) {
-                    return {
-                        status: CustomCommandStatus.Failure,
-                        message: e.message
+                system.run(() => {
+                    if (args[0] === null || args[0] === undefined || args[0] === '') {
+                        mainhand?.setLore();
+                    } else {
+                        const lore = mainhand?.getLore()
+                        lore?.push(args[0])
+                        mainhand?.setLore(lore)
                     }
-                }
 
+                    origin.sourceEntity?.getComponent(EntityComponentTypes.Equippable)?.setEquipment(EquipmentSlot.Mainhand, mainhand)
+                })
+            } catch (e: any) {
                 return {
                     status: CustomCommandStatus.Failure,
-                    message: "Something went wrong"
-                }
-            }
-
-            return {
-                status: CustomCommandStatus.Success
-            }
-        }
-    )
-
-    const removeLoreCommand: CustomCommand = {
-        name: "amethyst:loreremove",
-        description: "Remove all lore from the item you're holding",
-        permissionLevel: CommandPermissionLevel.Admin
-    };
-
-    initEvent.customCommandRegistry.registerCommand(
-        removeLoreCommand,
-        (origin, ...args) => {
-            try {
-                const mainhand = origin.initiator?.getComponent(EntityComponentTypes.Equippable)?.getEquipment(EquipmentSlot.Mainhand)
-
-                mainhand?.setLore();
-
-                origin.initiator?.getComponent(EntityComponentTypes.Equippable)?.setEquipment(EquipmentSlot.Mainhand, mainhand)
-            } catch (e: any) {
-                if (e instanceof ArgumentOutOfBoundsError) {
-                    return {
-                        status: CustomCommandStatus.Failure,
-                        message: e.message
-                    }
-                }
-
-                return {
-                    status: CustomCommandStatus.Failure,
-                    message: "Something went wrong"
+                    message: e.message
                 }
             }
 
