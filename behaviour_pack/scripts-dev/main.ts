a9f77cdff255ba8555c2a1995c34ae69ffe63208
Pavel Krutov <pavel@krutov.eu>
2026-03-01 01:47:04 +0000

    feat: add new lore and remove lore commands

--- behaviour_pack/scripts-dev/main.ts
+++ behaviour_pack/scripts-dev/main.ts
@@ -3,7 +3,16 @@ import load_loops from './loops'
 import load_world_event_handlers from './events';
 import {WorldCache} from "./api/sacrifice";
 import api from "./api";
-import {system} from "@minecraft/server";
+import {
+    CommandPermissionLevel,
+    CustomCommand,
+    CustomCommandParamType,
+    CustomCommandStatus, EntityComponentTypes,
+    EntityInventoryComponent, EquipmentSlot,
+    system
+} from "@minecraft/server";
+
+import {ArgumentOutOfBoundsError} from "@minecraft/common"
 
 const guild_id = process.env.GUILD_ID || '0'
 
@@ -34,12 +43,74 @@ system.beforeEvents.startup.subscribe(initEvent => {
     const loreCommand: CustomCommand = {
         name: "amethyst:loreadd",
         description: "Add one line of lore to the item you're holding",
-        permissionLevel: CustomCommandPermissionLevel.Admin,
-        optionalParameters: [{ type: CustomCommandParamType.Integer, name: "celebrationSize" }],
+        permissionLevel: CommandPermissionLevel.Admin,
+        mandatoryParameters: [{ type: CustomCommandParamType.String, name: "text" }],
     };
+
     initEvent.customCommandRegistry.registerCommand(
         loreCommand,
-        (origin, args) => {}
+        (origin, ...args) => {
+            try {
+                const mainhand = origin.initiator?.getComponent(EntityComponentTypes.Equippable)?.getEquipment(EquipmentSlot.Mainhand)
+
+                const lore = mainhand?.getLore()
+                lore?.push(args[0])
+                mainhand?.setLore(lore)
+
+                origin.initiator?.getComponent(EntityComponentTypes.Equippable)?.setEquipment(EquipmentSlot.Mainhand, mainhand)
+            } catch (e: any) {
+                if (e instanceof ArgumentOutOfBoundsError) {
+                    return {
+                        status: CustomCommandStatus.Failure,
+                        message: e.message
+                    }
+                }
+
+                return {
+                    status: CustomCommandStatus.Failure,
+                    message: "Something went wrong"
+                }
+            }
+
+            return {
+                status: CustomCommandStatus.Success
+            }
+        }
+    )
+
+    const removeLoreCommand: CustomCommand = {
+        name: "amethyst:loreremove",
+        description: "Remove all lore from the item you're holding",
+        permissionLevel: CommandPermissionLevel.Admin
+    };
+
+    initEvent.customCommandRegistry.registerCommand(
+        removeLoreCommand,
+        (origin, ...args) => {
+            try {
+                const mainhand = origin.initiator?.getComponent(EntityComponentTypes.Equippable)?.getEquipment(EquipmentSlot.Mainhand)
+
+                mainhand?.setLore();
+
+                origin.initiator?.getComponent(EntityComponentTypes.Equippable)?.setEquipment(EquipmentSlot.Mainhand, mainhand)
+            } catch (e: any) {
+                if (e instanceof ArgumentOutOfBoundsError) {
+                    return {
+                        status: CustomCommandStatus.Failure,
+                        message: e.message
+                    }
+                }
+
+                return {
+                    status: CustomCommandStatus.Failure,
+                    message: "Something went wrong"
+                }
+            }
+
+            return {
+                status: CustomCommandStatus.Success
+            }
+        }
     )
 
 })
