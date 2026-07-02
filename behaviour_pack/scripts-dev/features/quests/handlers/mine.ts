028eff0ee24a09694650b73e0d8a2c51302201b4
Pavel <pbkrutov@gmail.com>
2026-07-02 11:50:28 +0300

    feat: integrate interaction tracking and enhance mine event handling
    - Added interaction checks via `listInteractionsV1GuildsMeInteractionsGet` for mining events.
    - Updated `playerBreakBlock` to include `naturally_mined` detection logic.
    - Incorporated `QUEST_PROGRESS_CACHE` to track quest progress more efficiently.

--- behaviour_pack/scripts-dev/features/quests/handlers/mine.ts
+++ behaviour_pack/scripts-dev/features/quests/handlers/mine.ts
@@ -1,14 +1,25 @@
 import {world} from "@minecraft/server";
 import {MineAction} from "../core/action";
+import {listInteractionsV1GuildsMeInteractionsGet} from "../../../api/nexuscore/guilds/guilds";
+import api from "../../../api";
+import {QUEST_PROGRESS_CACHE} from "../progress-cache";
 
 export default function loadMineHandler() {
-    world.afterEvents.playerBreakBlock.subscribe((event) => {
+    world.afterEvents.playerBreakBlock.subscribe(async (event) => {
+        const interactions = await listInteractionsV1GuildsMeInteractionsGet({
+            coordinates: [event.block.x, event.block.y, event.block.z]
+        })
+
+        const thorny_user = api.ThornyUser.fetch_user(event.player.name)!
+        const quest_progress = QUEST_PROGRESS_CACHE.get(thorny_user.thorny_id)
+
         const mining_action: MineAction = {
             time: new Date(),
             coordinates: event.block.location,
             dimension: event.player.dimension.id,
             block_id: event.block.typeId,
             mainhand: event.itemStackBeforeBreak?.typeId ?? null,
+            naturally_mined: interactions.length > 1 // Breaking a block for the first time adds one interaction, so check for >1
         }
     })
 }
