5619587cb037d98285de834c86d4987a4fd1eb9f
Pavel <pbkrutov@gmail.com>
2026-07-03 16:31:18 +0300

    refactor: remove deprecated quest loop and outdated comments in quest feature loader

--- behaviour_pack/scripts-dev/features/quests/index.ts
+++ behaviour_pack/scripts-dev/features/quests/index.ts
@@ -5,8 +5,6 @@ import loadKillHandler from "./handlers/kill";
 import loadWriteBackLoop from "./write-back";
 
 export default function loadQuestsFeature() {
-    // Quest definitions must be loaded before progress, since progress-cache
-    // reads from QUEST_CACHE when activating objectives on playerSpawn.
     loadQuestCache()
     loadQuestProgressCache()
     loadMineHandler()
